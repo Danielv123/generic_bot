e3c0f82a2899b3c01a7f399bf657cff0985f797a
Danielv123 <danielv@danielv.no>
2024-12-16 16:52:08 +0100

    Better cuda testing script

--- cudatest.py
+++ cudatest.py
@@ -1,2 +1,14 @@
 import cv2
+import torch
 print(cv2.getBuildInformation())
+
+print("CV2 GPU support:")
+count = cv2.cuda.getCudaEnabledDeviceCount()
+print(count)
+
+print("Pytorch cuda support:")
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+for i in range(torch.cuda.device_count()):
+    print(torch.cuda.get_device_name(i))
+    print(torch.cuda.get_device_properties(i))
